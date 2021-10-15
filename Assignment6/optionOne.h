e4de129090f58e7a653ad6949c021aa5b53e367a
NhanThai0501 <72413541+NhanThai0501@users.noreply.github.com>
2021-10-14 19:41:10 -0700

    Update optionOne.h
    add pre and post condition

--- Assignment6/optionOne.h
+++ Assignment6/optionOne.h
@@ -6,6 +6,8 @@
 #include "IntegerBag.h"
 #include "input.h"
 
+// Precondition: valid IntegerBag object
+// Postcondition: search the value from MyBag and display where it is
 void searchIntValue(IntegerBag MyBag)
 {
 	if (MyBag.isEmpty())
@@ -23,6 +25,8 @@ void searchIntValue(IntegerBag MyBag)
 		cout << "\n\t\tValue " << value << " is found at subscript #" << atIndex << " from MyBag.\n" << endl;
 }
 
+// Precondition: valid IntegerBag object
+// Postcondition: clear all elements in MyBag
 void emptyIntBag(IntegerBag &MyBag)
 {
 	if (MyBag.isEmpty())
@@ -35,13 +39,17 @@ void emptyIntBag(IntegerBag &MyBag)
 	cout << "\n\t\tMyBag is cleared of all elements.\n" << endl;
 }
 
+// Precondition: valid reference IntegerBag object
+// Postcondition: enter a value and insert it into MyBag
 void insertInt(IntegerBag &MyBag)
 {
 	int intInserted = inputInteger("\n\t\tEnter a value and insert into MyBag: ");
 	MyBag.insert(intInserted);
-	cout << "\n\t\t" << intInserted << " has been inserted into MyBag\n" << endl;
+	cout << "\n\t\t" << intInserted << " has been inserted into MyBag.\n" << endl;
 }
 
+// Precondition: valid IntegerBag object
+// Postcondition: enter the MyBag's index and remove value from it.
 void removeIntIndex(IntegerBag &MyBag)
 {
 	if (MyBag.isEmpty())
@@ -55,9 +63,11 @@ void removeIntIndex(IntegerBag &MyBag)
 	int valueRemoved = MyBag.getValueAt(indexRemoved);
 	MyBag.remove(indexRemoved);
 
-	cout << "\n\t\tValue " << valueRemoved << " has been deleted from MyBag\n" << endl;
+	cout << "\n\t\tValue " << valueRemoved << " has been deleted from MyBag.\n" << endl;
 }
 
+// Precondition: valid IntegerBag object
+// Postcondition: sort the value in MyBag
 void sortInt(IntegerBag &MyBag)
 {
 	if (MyBag.isEmpty())
@@ -67,9 +77,11 @@ void sortInt(IntegerBag &MyBag)
 	}
 
 	MyBag.sort();
-	cout << "\n\t\tMyBag has been sorted\n" << endl;
+	cout << "\n\t\tMyBag has been sorted.\n" << endl;
 }
 
+// Precondition: valid IntegerBag object
+// Postcondition: display all contents in MyBag
 void displayIntBag(IntegerBag MyBag)
 {
 	if (MyBag.isEmpty())
